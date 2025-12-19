fcef999e39bab9aeb85c35a38f7951f8f9feda93
oruehl <oliverruehl@mail.de>
2025-12-19 20:54:34 +0100

    I added the best version which only runs for Godot 2. To get to this point again is my goal.

--- src/objects/rewards/random_coin.gd
+++ src/objects/rewards/random_coin.gd
@@ -1,6 +1,12 @@
 signal taken
 
-#@onready var anim = get_node("anim")
+@onready var anim = get_node("anim")
+@onready var sfx = get_node("sfx")
 
 func _ready():
 	pass
+
+
+func play_taken_animation():
+	anim.play("taken")
+	sfx.play()
--- src/objects/rewards/reward.gd
+++ src/objects/rewards/reward.gd
@@ -3,3 +3,14 @@ extends Area2D
 @onready var coin = get_node("random_coin")
 
 var destroyed = false
+
+
+func destroy():
+	if destroyed:
+		return
+
+	destroyed = true
+	coin.play_taken_animation()
+	Game.collect_item()
+	await get_tree().create_timer(0.5).timeout
+	queue_free()
